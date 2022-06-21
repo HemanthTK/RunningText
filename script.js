152f259c3d57e14b39326f6b08c7086a201e2454
Hemanth T K <hemanth.nmit16g28@gmail.com>
2022-06-21 21:56:16 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const textEl=document.getElementById("text");
+const speedEl=document.getElementById("speed");
+
+const text="We love Programming!";
+
+let idx=1;
+const NUM=500;
+
+let time=NUM/speedEl.value;
+
+writeText();
+
+function writeText(){
+    textEl.innerText=text.slice(0,idx);
+    idx++;
+
+    if(idx>text.length)
+     idx=1;
+
+     setTimeout(writeText,time);
+
+}
+
+speedEl.addEventListener("input",(e)=>{
+    time=NUM/e.target.value;
+})
